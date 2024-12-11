812e2bd07ac303b6fbd6dc1887be006f954dde9c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-12-10 18:20:07 -0600

    feat: Handle command line arguments
    The user can now change the behavior of the program by passing command
    line arguments. The currently supported arguments are `--left`,
    `--right`, and `--center`. The user can also use the shortened versions
    of these arguments.

--- text-generation/text-alignment/main.c
+++ text-generation/text-alignment/main.c
@@ -4,7 +4,7 @@
 
 enum { ALIGN_LEFT, ALIGN_RIGHT, ALIGN_CENTER };
 
-void process_line(char *line, int width, int justify) {
+void process_line(char *line, int width, int align) {
   int len = strlen(line);
   int startOfLine = 0;
 
@@ -16,11 +16,11 @@ void process_line(char *line, int width, int justify) {
         spaces++;
       }
 
-      if (justify == ALIGN_RIGHT) {
+      if (align == ALIGN_RIGHT) {
         for (int j = 0; j < width - (i - startOfLine); j++) {
           putchar(' ');
         }
-      } else if (justify == ALIGN_CENTER) {
+      } else if (align == ALIGN_CENTER) {
         int padding = (width - (i - startOfLine)) / 2;
         for (int j = 0; j < padding; j++) {
           putchar(' ');
@@ -39,14 +39,14 @@ void process_line(char *line, int width, int justify) {
   }
 
   if (startOfLine < len) {
-    if (justify == ALIGN_LEFT) {
+    if (align == ALIGN_LEFT) {
       fwrite(line + startOfLine, len - startOfLine, 1, stdout);
-    } else if (justify == ALIGN_RIGHT) {
+    } else if (align == ALIGN_RIGHT) {
       for (int j = 0; j < width - (len - startOfLine); j++) {
         putchar(' ');
       }
       fwrite(line + startOfLine, len - startOfLine, 1, stdout);
-    } else if (justify == ALIGN_CENTER) {
+    } else if (align == ALIGN_CENTER) {
       int padding = (width - (len - startOfLine)) / 2;
       for (int j = 0; j < padding; j++) {
         putchar(' ');
@@ -57,13 +57,35 @@ void process_line(char *line, int width, int justify) {
   }
 }
 
-int main() {
+void usage(const char *name) {
+  fprintf(stderr, "Usage: %s [OPTIONS]\n"
+      "\n"
+      "Options:\n"
+      "  -l, --left          Align text to the left (default)\n"
+      "  -r, --right         Align text to the right\n"
+      "  -c, --center        Center align text\n"
+      "\n", name);
+  exit(EXIT_FAILURE);
+}
+
+int main(int argc, char *argv[]) {
   int width = 80;
   char *line = NULL;
   size_t length = 0;
+  int align = ALIGN_LEFT;
+
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--left") == 0) {
+      align = ALIGN_LEFT;
+    } else if (strcmp(argv[i], "-r") == 0 || strcmp(argv[i], "--right") == 0) {
+      align = ALIGN_RIGHT;
+    } else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--center") == 0) {
+      align = ALIGN_CENTER;
+    }
+  }
 
   while (getline(&line, &length, stdin) != -1) {
-    process_line(line, width, ALIGN_CENTER);
+    process_line(line, width, align);
   }
 
   free(line);
