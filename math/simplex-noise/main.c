0e17d38c5b8829c32aa0c66358274e8ee8a6b31c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-01-06 18:53:56 -0600

    feat: Implement simplex noise algorithm
    This commit implements the simplex noise algorithm. The main resource I
    used to implement this is the following PDF:
    
    [https://muugumuugu.github.io/bOOkshelF/generative%20art/simplexnoise.pdf]

--- math/simplex-noise/main.c
+++ math/simplex-noise/main.c
@@ -29,7 +29,64 @@ double grad3[][3] = {{1, 1, 0}, {-1, 1, 0}, {1, -1, 0}, {-1, -1, 0},
 double dot(double g[], double x, double y) { return g[0] * x + g[1] * y; }
 
 double noise(double x, double y) {
-  return 0;
+  const double F2 = 0.5 * (sqrt(3.0) - 1.0);
+  const double G2 = (3.0 - sqrt(3.0)) / 6.0;
+
+  double s = (x + y) * F2;
+  int i = floor(x + s);
+  int j = floor(y + s);
+  double t = (i + j) * G2;
+  double X0 = i - t;
+  double Y0 = j - t;
+  double x0 = x - X0;
+  double y0 = y - Y0;
+
+  int i1, j1;
+  if (x0 > y0) {
+    i1 = 1;
+    j1 = 0;
+  } else {
+    i1 = 0;
+    j1 = 1;
+  }
+
+  double x1 = x0 - i1 + G2;
+  double y1 = y0 - j1 + G2;
+  double x2 = x0 - 1.0 + 2.0 * G2;
+  double y2 = y0 - 1.0 + 2.0 * G2;
+
+  int ii = i & 255;
+  int jj = j & 255;
+  int gi0 = perm[ii + perm[jj]] % 12;
+  int gi1 = perm[ii + i1 + perm[jj + j1]] % 12;
+  int gi2 = perm[ii + 1 + perm[jj + 1]] % 12;
+
+  double n0, n1, n2;
+  double t0 = 0.5 - x0 * x0 - y0 * y0;
+  if (t0 < 0)
+    n0 = 0.0;
+  else {
+    t0 *= t0;
+    n0 = t0 * t0 * dot(grad3[gi0], x0, y0);
+  }
+
+  double t1 = 0.5 - x1 * x1 - y1 * y1;
+  if (t1 < 0)
+    n1 = 0.0;
+  else {
+    t1 *= t1;
+    n1 = t1 * t1 * dot(grad3[gi1], x1, y1);
+  }
+
+  double t2 = 0.5 - x2 * x2 - y2 * y2;
+  if (t2 < 0)
+    n2 = 0.0;
+  else {
+    t2 *= t2;
+    n2 = t2 * t2 * dot(grad3[gi2], x2, y2);
+  }
+
+  return 70.0 * (n0 + n1 + n2);
 }
 
 void drawImage(int width, int height) {
