8a09792b2e6a348d2b114e39f7dfad91f4cf4f6e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-01-07 18:58:15 -0600

    Add scale parameter to the drawImage function
    The `drawImage` function now has a scale parameter which can be used to
    "zoom" in and out the noise.

--- math/simplex-noise/main.c
+++ math/simplex-noise/main.c
@@ -89,16 +89,13 @@ double noise(double x, double y) {
   return 70.0 * (n0 + n1 + n2);
 }
 
-void drawImage(int width, int height) {
+void drawImage(int width, int height, float scale) {
   printf("P3\n%d %d\n255\n", width, height);
   for (int y = 0; y < height; y++) {
     for (int x = 0; x < width; x++) {
-      double value = noise(x / 100.0, y / 100.0);
-      if (value < 0) {
-        printf("255 255 255\n");
-      } else {
-        printf("0 0 0\n");
-      }
+      double value = noise(x * scale, y * scale);
+      int s = (int)(value * 128 + 128);
+      printf("%d %d %d ", s, s, s);
     }
     printf("\n");
   }
@@ -110,5 +107,5 @@ int main() {
     perm[i + 256] = p[i];
   }
 
-  drawImage(512, 512);
+  drawImage(512, 512, 0.05);
 }
