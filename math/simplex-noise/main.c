659887d69e16eb49da67d06912bf58cf53e6e72f
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-01-23 18:48:16 -0600

    refactor: Rewrite the noise function using Vec2
    The `Vec2` struct is a better way to represent a position. I have used
    this to refactor the `noise` function.

--- math/simplex-noise/main.c
+++ math/simplex-noise/main.c
@@ -55,26 +55,23 @@ typedef struct {
   double y;
 } Vec2;
 
-double dot(double g[], Vec2 xy) { return g[0] * xy.x + g[1] * xy.y; }
+typedef StyleFunc Color (*)(Vec2, int, bool, Range);
+
+double dot(double g[], Vec2 v) { return g[0] * v.x + g[1] * v.y; }
 
 double noise(Vec2 pos) {
-  double x = pos.x;
-  double y = pos.y;
-
   const double F2 = 0.5 * (sqrt(3.0) - 1.0);
   const double G2 = (3.0 - sqrt(3.0)) / 6.0;
 
-  double s = (x + y) * F2;
-  int i = floor(x + s);
-  int j = floor(y + s);
+  double s = (pos.x + pos.y) * F2;
+  int i = floor(pos.x + s);
+  int j = floor(pos.y + s);
   double t = (i + j) * G2;
-  double X0 = i - t;
-  double Y0 = j - t;
-  double x0 = x - X0;
-  double y0 = y - Y0;
+
+  Vec2 v0 = {pos.x - (i - t), pos.y - (j - t)};
 
   int i1, j1;
-  if (x0 > y0) {
+  if (v0.x > v0.y) {
     i1 = 1;
     j1 = 0;
   } else {
@@ -82,10 +79,8 @@ double noise(Vec2 pos) {
     j1 = 1;
   }
 
-  double x1 = x0 - i1 + G2;
-  double y1 = y0 - j1 + G2;
-  double x2 = x0 - 1.0 + 2.0 * G2;
-  double y2 = y0 - 1.0 + 2.0 * G2;
+  Vec2 v1 = {v0.x - i1 + G2, v0.y - j1 + G2};
+  Vec2 v2 = {v0.x - 1.0 + 2.0 * G2, v0.y - 1.0 + 2.0 * G2};
 
   int ii = i & 255;
   int jj = j & 255;
@@ -94,28 +89,29 @@ double noise(Vec2 pos) {
   int gi2 = perm[ii + 1 + perm[jj + 1]] % 12;
 
   double n0, n1, n2;
-  double t0 = 0.5 - x0 * x0 - y0 * y0;
+
+  double t0 = 0.5 - v0.x * v0.x - v0.y * v0.y;
   if (t0 < 0) {
     n0 = 0.0;
   } else {
     t0 *= t0;
-    n0 = t0 * t0 * dot(grad3[gi0], (Vec2){x0, y0});
+    n0 = t0 * t0 * dot(grad3[gi0], v0);
   }
 
-  double t1 = 0.5 - x1 * x1 - y1 * y1;
+  double t1 = 0.5 - v1.x * v1.x - v1.y * v1.y;
   if (t1 < 0) {
     n1 = 0.0;
   } else {
     t1 *= t1;
-    n1 = t1 * t1 * dot(grad3[gi1], (Vec2){x1, y1});
+    n1 = t1 * t1 * dot(grad3[gi1], v1);
   }
 
-  double t2 = 0.5 - x2 * x2 - y2 * y2;
+  double t2 = 0.5 - v2.x * v2.x - v2.y * v2.y;
   if (t2 < 0) {
     n2 = 0.0;
   } else {
     t2 *= t2;
-    n2 = t2 * t2 * dot(grad3[gi2], (Vec2){x2, y2});
+    n2 = t2 * t2 * dot(grad3[gi2], v2);
   }
 
   return 70.0 * (n0 + n1 + n2);
