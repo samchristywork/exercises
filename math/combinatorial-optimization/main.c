77812787ac31f63ed1738d3e71b8568625c6a064
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-02-21 18:35:43 -0600

    feat: implement early return for special cases
    The special cases of `n==0`, `capacity==0`, and `n==1` are trivial to
    solve, and don't require the full knapsack algorithm.

--- math/combinatorial-optimization/main.c
+++ math/combinatorial-optimization/main.c
@@ -9,6 +9,27 @@ typedef struct Item {
 
 Item *knapsack(Item items[], int n, int capacity, int *max_value,
                int *remaining_capacity, int *n_items) {
+  if (n == 0 || capacity == 0) {
+    *max_value = 0;
+    *remaining_capacity = capacity;
+    *n_items = 0;
+    return NULL;
+  }
+
+  if (n==1) {
+    Item *selected_items = (Item *)malloc(1 * sizeof(Item));
+    if (items[0].weight <= capacity) {
+      selected_items[0] = items[0];
+    } else {
+      selected_items[0].weight = 0;
+      selected_items[0].value = 0;
+    }
+    *max_value = selected_items[0].value;
+    *remaining_capacity = capacity - selected_items[0].weight;
+    *n_items = 1;
+    return selected_items;
+  }
+
   // Create a 2D array to store the maximum value for each capacity
   int **dp = (int **)malloc((n + 1) * sizeof(int *));
   for (int i = 0; i <= n; i++) {
