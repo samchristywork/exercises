ff92cdf4fbc2db13efa11aff05363dae8a5ac1c0
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-03-20 20:49:24 -0500

    feat: Add worker struct to web server
    The workers will be in charge of fulfilling all requests. They each
    spawn threads when they are instantiated, and then hold those threads
    through their lifetimes so that the threads don't have to be created and
    destroyed each time a new request comes in.

---
+++ web/thread-pool-server/src/main.rs
@@ -0,0 +1,35 @@
+use std::io::prelude::*;
+use std::thread;
+use std::sync::{Arc, Mutex, mpsc};
+
+struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job.call_box();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+fn main() {
+}
