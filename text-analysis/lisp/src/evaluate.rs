03bbb1c4defe79e1414d1460506af052efad0f75
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-03-30 19:11:46 -0500

    feat: Add missing intrinsics to handle_symbol
    This commit adds all of the intrinsics listed in the `intrinsic` module.
    
    I have also organized all of them into categories.

--- text-analysis/lisp/src/evaluate.rs
+++ text-analysis/lisp/src/evaluate.rs
@@ -3,34 +3,102 @@ use crate::Node;
 use crate::TokenKind;
 use crate::intrinsic;
 
+pub fn evaluate_node(node: &Node, env: &mut Environment) -> Node {
+    match node.token.kind {
+        TokenKind::Module => {
+            for child in &node.children {
+                evaluate_node(child, env);
+            }
+
+            Node {
+                token: node.token.clone(),
+                children: Vec::new(),
+            }
+        }
+        TokenKind::LParen => {
+            let function = evaluate_node(&node.children[0], env);
+            apply_function(&function, &node.children[1..], env)
+        }
+        TokenKind::Symbol => env
+            .get(&node.token.value)
+            .map_or_else(|| node.clone(), std::clone::Clone::clone),
+        _ => node.clone(),
+    }
+}
+
+fn handle_symbol(function: &Node, args: &[Node], env: &mut Environment) -> Node {
+    match function.token.value.as_str() {
+        // Operators
+        "+" => intrinsic::fn_add(args, env),
+        "-" => intrinsic::fn_sub(args, env),
+        "*" => intrinsic::fn_mul(args, env),
+        "=" => intrinsic::fn_equal(args, env),
+        "<" => intrinsic::fn_less_than(args, env),
+        ">" => intrinsic::fn_greater_than(args, env),
+        "^" => intrinsic::fn_pow(args, env),
+        "%" => intrinsic::fn_mod(args, env),
+        "++" => intrinsic::fn_inc(args, env),
+        "--" => intrinsic::fn_dec(args, env),
+        "max" => intrinsic::fn_max(args, env),
+        "min" => intrinsic::fn_min(args, env),
+
+        // Types
+        "text?" => intrinsic::fn_is_text(args, env),
+        "number?" => intrinsic::fn_is_number(args, env),
+        "symbol?" => intrinsic::fn_is_symbol(args, env),
+        "lparen?" => intrinsic::fn_is_lparen(args, env),
+        "lambda?" => intrinsic::fn_is_lambda(args, env),
+        "atom?" => intrinsic::fn_is_atom(args, env),
+
+        // Logical
+        "and" => intrinsic::fn_and(args, env),
+        "or" => intrinsic::fn_or(args, env),
+
+        // Control flow
+        "if" => intrinsic::fn_if(args, env),
+        "cond" => intrinsic::fn_cond(args, env),
+        "repeat" => intrinsic::fn_repeat(args, env),
+        "loop" => intrinsic::fn_loop(args, env),
+        "true" => intrinsic::fn_true(),
+        "false" => intrinsic::fn_false(),
+
+        // I/O
+        "print" => intrinsic::fn_print(args, env),
+        "read-line" => intrinsic::fn_read_line(),
+
+        // Strings
+        "join" => intrinsic::fn_join(args, env),
+
+        // Environment
+        "print-env" => intrinsic::fn_print_env(args, env),
+
+        // Assignment
+        "def" => intrinsic::fn_def(args, env),
+        "defun" => intrinsic::fn_defun(args, env),
+
+        // Higher-order functions
+        "map" => intrinsic::fn_map(args, env),
+        "filter" => intrinsic::fn_filter(args, env),
+
+        // Lists
+        "list" => intrinsic::fn_list(args, env),
+        "head" => intrinsic::fn_head(args, env),
+        "tail" => intrinsic::fn_tail(args, env),
+        "length" => intrinsic::fn_length(args, env),
+
+        // Miscellaneous
+        "even?" => intrinsic::fn_is_even(args, env),
+        "odd?" => intrinsic::fn_is_odd(args, env),
+        _ => env.get(&function.token.value).map_or_else(
+            || panic!("Unknown function: {}", function.token.value),
+            std::clone::Clone::clone,
+        ),
+    }
+}
+
 fn apply_function(function: &Node, args: &[Node], env: &mut Environment) -> Node {
     match function.token.kind {
-        TokenKind::Symbol => match function.token.value.as_str() {
-            "+" => intrinsic::fn_add(args, env),
-            "-" => intrinsic::fn_sub(args, env),
-            "*" => intrinsic::fn_mul(args, env),
-            "repeat" => intrinsic::fn_repeat(args, env),
-            "loop" => intrinsic::fn_loop(args, env),
-            "print" => intrinsic::fn_print(args, env),
-            "join" => intrinsic::fn_join(args, env),
-            "print-env" => intrinsic::fn_print_env(args, env),
-            "true" => intrinsic::fn_true(),
-            "false" => intrinsic::fn_false(),
-            "=" => intrinsic::fn_equal(args, env),
-            "if" => intrinsic::fn_if(args, env),
-            "cond" => intrinsic::fn_cond(args, env),
-            "<" => intrinsic::fn_less_than(args, env),
-            ">" => intrinsic::fn_greater_than(args, env),
-            "def" => intrinsic::fn_def(args, env),
-            "defun" => intrinsic::fn_defun(args, env),
-            "read-line" => intrinsic::fn_read_line(),
-            "list" => intrinsic::fn_list(args, env),
-            "map" => intrinsic::fn_map(args, env),
-            _ => env.get(&function.token.value).map_or_else(
-                || panic!("Unknown function: {}", function.token.value),
-                std::clone::Clone::clone,
-            ),
-        },
+        TokenKind::Symbol => handle_symbol(function, args, env),
         TokenKind::Lambda => {
             let params = &function.children[0];
             let body = &function.children[1..];
@@ -53,28 +121,5 @@ fn apply_function(function: &Node, args: &[Node], env: &mut Environment) -> Node
             apply_function(&function.children[0].clone(), &function.children[1..], env)
         }
         _ => panic!("Invalid function application"),
-    }
-}
-
-pub fn evaluate_node(node: &Node, env: &mut Environment) -> Node {
-    match node.token.kind {
-        TokenKind::Module => {
-            for child in &node.children {
-                evaluate_node(child, env);
-            }
-
-            Node {
-                token: node.token.clone(),
-                children: Vec::new(),
-            }
-        }
-        TokenKind::LParen => {
-            let function = evaluate_node(&node.children[0], env);
-            apply_function(&function, &node.children[1..], env)
-        }
-        TokenKind::Symbol => env
-            .get(&node.token.value)
-            .map_or_else(|| node.clone(), std::clone::Clone::clone),
-        _ => node.clone(),
     }
 }
