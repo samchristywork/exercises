7b91aef7105839021e23f8b899beaf8485706593
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-25 21:44:59 -0500

    feat: Add add function
    This function computes the results of adding two arbitrary precision
    numbers.
    
    The memory needs to be freed by the caller.

--- math/arbitrary-precision-arithmetic/main.c
+++ math/arbitrary-precision-arithmetic/main.c
@@ -30,5 +30,36 @@ void free_number(Number number) {
   number.length = 0;
 }
 
+Number add(Number a, Number b) {
+  int maxLength = (a.length > b.length) ? a.length : b.length;
+  Number result;
+  result.length = maxLength + 1;
+  result.digits = allocate_string(result.length + 1);
+  result.digits[result.length] = '\0';
+
+  int carry = 0;
+
+  for (int i = 0; i < maxLength; i++) {
+    int digitA = (i < a.length) ? a.digits[a.length - 1 - i] - '0' : 0;
+    int digitB = (i < b.length) ? b.digits[b.length - 1 - i] - '0' : 0;
+
+    int sum = digitA + digitB + carry;
+    result.digits[result.length - 2 - i] = (sum % 10) + '0';
+    carry = sum / 10;
+  }
+
+  if (carry > 0) {
+    result.digits[0] = carry + '0';
+  } else {
+    result.length--;
+    for (int i = 0; i < result.length; i++) {
+      result.digits[i] = result.digits[i + 1];
+    }
+    result.digits = (char*)realloc(result.digits, result.length + 1);
+  }
+
+  return result;
+}
+
 int main() {
 }
