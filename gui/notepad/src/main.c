b20e09a62606dc94ecd63acdb8863e220aea95d8
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-30 18:37:06 -0500

    feat: Add functions for handling saving files
    These functions allow the user to save a file. The program opens a save file
    dialog and names the file accordingly.

--- gui/notepad/src/main.c
+++ gui/notepad/src/main.c
@@ -69,5 +69,82 @@ void open_file(AppData *data) {
   gtk_widget_show_all(data->open_dialog);
 }
 
+void save_handler(GtkWidget *dialog, gint response_id, AppData *data) {
+  if (response_id == GTK_RESPONSE_ACCEPT) {
+    char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+    if (filename) {
+      GtkTextBuffer *text_buffer =
+          gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
+      GtkTextIter start, end;
+      gtk_text_buffer_get_start_iter(text_buffer, &start);
+      gtk_text_buffer_get_end_iter(text_buffer, &end);
+      char *text = gtk_text_buffer_get_text(text_buffer, &start, &end, TRUE);
+
+      FILE *file = fopen(filename, "w");
+      if (file) {
+        fprintf(file, "%s", text);
+        fclose(file);
+        g_free(text);
+
+        if (data->filename) {
+          g_free(data->filename);
+        }
+        data->filename = g_strdup(filename);
+        data->modified = FALSE;
+      } else {
+        g_print("Error saving file.\n");
+        g_free(text);
+      }
+
+      g_free(filename);
+    }
+  }
+
+  gtk_widget_destroy(dialog);
+  data->save_dialog = NULL;
+}
+
+void save_as_file(AppData *data) {
+  if (data->save_dialog) {
+    gtk_widget_destroy(data->save_dialog);
+  }
+
+  data->save_dialog = gtk_file_chooser_dialog_new(
+      "Save File As", GTK_WINDOW(data->window), GTK_FILE_CHOOSER_ACTION_SAVE,
+      "_Cancel", GTK_RESPONSE_CANCEL, "_Save", GTK_RESPONSE_ACCEPT, NULL);
+
+  gtk_file_chooser_set_do_overwrite_confirmation(
+      GTK_FILE_CHOOSER(data->save_dialog), TRUE);
+
+  g_signal_connect(data->save_dialog, "response", G_CALLBACK(save_handler),
+                   data);
+
+  gtk_widget_show_all(data->save_dialog);
+}
+
+void save_file(AppData *data) {
+  if (data->filename) {
+    GtkTextBuffer *text_buffer =
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
+    GtkTextIter start, end;
+    gtk_text_buffer_get_start_iter(text_buffer, &start);
+    gtk_text_buffer_get_end_iter(text_buffer, &end);
+    char *text = gtk_text_buffer_get_text(text_buffer, &start, &end, TRUE);
+
+    FILE *file = fopen(data->filename, "w");
+    if (file) {
+      fprintf(file, "%s", text);
+      fclose(file);
+      g_free(text);
+      data->modified = FALSE;
+    } else {
+      g_print("Error saving file.\n");
+      g_free(text);
+    }
+  } else {
+    save_as_file(data);
+  }
+}
+
 int main(int argc, char *argv[]) {
 }
