1980e39c59cbd157eaa9957d0426567fc108ad9a
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-04-30 18:35:51 -0500

    feat: Add functions for handling opening files
    These functions are called when the user wants to open a new file. The functions
    spawn a file dialog and call `load_file` with the user response.

--- gui/notepad/src/main.c
+++ gui/notepad/src/main.c
@@ -38,5 +38,36 @@ void load_file(AppData *data, const char *filename) {
   }
 }
 
+void open_handler(GtkWidget *dialog, gint response_id, AppData *data) {
+  if (response_id == GTK_RESPONSE_ACCEPT) {
+    char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+    if (filename) {
+      load_file(data, filename);
+      g_free(filename);
+    }
+  }
+
+  gtk_widget_destroy(dialog);
+  data->open_dialog = NULL;
+}
+
+void open_file(AppData *data) {
+  if (data->open_dialog) {
+    gtk_widget_destroy(data->open_dialog);
+  }
+
+  data->open_dialog = gtk_file_chooser_dialog_new(
+      "Open File", GTK_WINDOW(data->window), GTK_FILE_CHOOSER_ACTION_OPEN,
+      "_Cancel", GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL);
+
+  gtk_file_chooser_set_do_overwrite_confirmation(
+      GTK_FILE_CHOOSER(data->open_dialog), TRUE);
+
+  g_signal_connect(data->open_dialog, "response", G_CALLBACK(open_handler),
+                   data);
+
+  gtk_widget_show_all(data->open_dialog);
+}
+
 int main(int argc, char *argv[]) {
 }
