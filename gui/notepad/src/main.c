05ecc4a7539bed984b3c959d3a54ccf6488f99ea
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-05-03 18:20:31 -0500

    fix: Avoid NULL pointer error
    The `AppData` struct can't be `NULL` if is global and has the same lifetime as
    the program.

--- gui/notepad/src/main.c
+++ gui/notepad/src/main.c
@@ -11,10 +11,9 @@ typedef struct {
   GtkWidget *font_selection_dialog;
   GtkWidget *status_bar;
 } AppData;
+AppData *app_data;
 
-void update_status_bar(AppData *data) {
-  GtkTextBuffer *buffer =
-      gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
+void update_status_bar(GtkTextBuffer *buffer, GtkWidget *status_bar) {
   GtkTextIter iter;
   gtk_text_buffer_get_iter_at_mark(buffer, &iter,
                                    gtk_text_buffer_get_insert(buffer));
@@ -23,10 +22,10 @@ void update_status_bar(AppData *data) {
 
   char status_text[100];
   sprintf(status_text, "Line: %d, Column: %d", line, column);
-  gtk_label_set_text(GTK_LABEL(data->status_bar), status_text);
+  gtk_label_set_text(GTK_LABEL(status_bar), status_text);
 }
 
-void load_file(AppData *data, const char *filename) {
+void load_file(const char *filename) {
   FILE *file = fopen(filename, "r");
   if (file) {
     fseek(file, 0, SEEK_END);
@@ -39,66 +38,66 @@ void load_file(AppData *data, const char *filename) {
     fclose(file);
 
     GtkTextBuffer *text_buffer =
-        gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(app_data->text_view));
     gtk_text_buffer_set_text(text_buffer, buffer, -1);
 
     g_free(buffer);
 
-    if (data->filename) {
-      g_free(data->filename);
+    if (app_data->filename) {
+      g_free(app_data->filename);
     }
-    data->filename = g_strdup(filename);
-    data->modified = FALSE;
+    app_data->filename = g_strdup(filename);
+    app_data->modified = FALSE;
   } else {
     g_print("Error opening file: %s\n", filename);
   }
 
   GtkTextBuffer *text_buffer =
-      gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
+      gtk_text_view_get_buffer(GTK_TEXT_VIEW(app_data->text_view));
   GtkTextIter start;
   gtk_text_buffer_get_start_iter(text_buffer, &start);
   gtk_text_buffer_place_cursor(text_buffer, &start);
 
-  update_status_bar(data);
+  update_status_bar(text_buffer, app_data->status_bar);
 }
 
-void open_handler(GtkWidget *dialog, gint response_id, AppData *data) {
+void open_handler(GtkWidget *dialog, gint response_id) {
   if (response_id == GTK_RESPONSE_ACCEPT) {
     char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
     if (filename) {
-      load_file(data, filename);
+      load_file(filename);
       g_free(filename);
     }
   }
 
   gtk_widget_destroy(dialog);
-  data->open_dialog = NULL;
+  app_data->open_dialog = NULL;
 }
 
-void open_file(AppData *data) {
-  if (data->open_dialog) {
-    gtk_widget_destroy(data->open_dialog);
+void open_file() {
+  if (app_data->open_dialog) {
+    gtk_widget_destroy(app_data->open_dialog);
   }
 
-  data->open_dialog = gtk_file_chooser_dialog_new(
-      "Open File", GTK_WINDOW(data->window), GTK_FILE_CHOOSER_ACTION_OPEN,
+  app_data->open_dialog = gtk_file_chooser_dialog_new(
+      "Open File", GTK_WINDOW(app_data->window), GTK_FILE_CHOOSER_ACTION_OPEN,
       "_Cancel", GTK_RESPONSE_CANCEL, "_Open", GTK_RESPONSE_ACCEPT, NULL);
 
   gtk_file_chooser_set_do_overwrite_confirmation(
-      GTK_FILE_CHOOSER(data->open_dialog), TRUE);
+      GTK_FILE_CHOOSER(app_data->open_dialog), TRUE);
 
-  g_signal_connect(data->open_dialog, "response", G_CALLBACK(open_handler),
-                   data);
+  g_signal_connect(app_data->open_dialog, "response", G_CALLBACK(open_handler),
+                   NULL);
 
-  gtk_widget_show_all(data->open_dialog);
+  gtk_widget_show_all(app_data->open_dialog);
 }
 
-void save_handler(GtkWidget *dialog, gint response_id, AppData *data) {
+void save_handler(GtkWidget *dialog, gint response_id) {
   if (response_id == GTK_RESPONSE_ACCEPT) {
     char *filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
     if (filename) {
       GtkTextBuffer *text_buffer =
-          gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
+          gtk_text_view_get_buffer(GTK_TEXT_VIEW(app_data->text_view));
       GtkTextIter start, end;
       gtk_text_buffer_get_start_iter(text_buffer, &start);
       gtk_text_buffer_get_end_iter(text_buffer, &end);
@@ -110,11 +109,11 @@ void save_handler(GtkWidget *dialog, gint response_id, AppData *data) {
         fclose(file);
         g_free(text);
 
-        if (data->filename) {
-          g_free(data->filename);
+        if (app_data->filename) {
+          g_free(app_data->filename);
         }
-        data->filename = g_strdup(filename);
-        data->modified = FALSE;
+        app_data->filename = g_strdup(filename);
+        app_data->modified = FALSE;
       } else {
         g_print("Error saving file.\n");
         g_free(text);
@@ -125,70 +124,71 @@ void save_handler(GtkWidget *dialog, gint response_id, AppData *data) {
   }
 
   gtk_widget_destroy(dialog);
-  data->save_dialog = NULL;
+  app_data->save_dialog = NULL;
 }
 
-void save_as_file(AppData *data) {
-  if (data->save_dialog) {
-    gtk_widget_destroy(data->save_dialog);
+void save_as_file() {
+  if (app_data->save_dialog) {
+    gtk_widget_destroy(app_data->save_dialog);
   }
 
-  data->save_dialog = gtk_file_chooser_dialog_new(
-      "Save File As", GTK_WINDOW(data->window), GTK_FILE_CHOOSER_ACTION_SAVE,
-      "_Cancel", GTK_RESPONSE_CANCEL, "_Save", GTK_RESPONSE_ACCEPT, NULL);
+  app_data->save_dialog = gtk_file_chooser_dialog_new(
+      "Save File As", GTK_WINDOW(app_data->window),
+      GTK_FILE_CHOOSER_ACTION_SAVE, "_Cancel", GTK_RESPONSE_CANCEL, "_Save",
+      GTK_RESPONSE_ACCEPT, NULL);
 
   gtk_file_chooser_set_do_overwrite_confirmation(
-      GTK_FILE_CHOOSER(data->save_dialog), TRUE);
+      GTK_FILE_CHOOSER(app_data->save_dialog), TRUE);
 
-  g_signal_connect(data->save_dialog, "response", G_CALLBACK(save_handler),
-                   data);
+  g_signal_connect(app_data->save_dialog, "response", G_CALLBACK(save_handler),
+                   NULL);
 
-  gtk_widget_show_all(data->save_dialog);
+  gtk_widget_show_all(app_data->save_dialog);
 }
 
-void save_file(AppData *data) {
-  if (data->filename) {
+void save_file() {
+  if (app_data->filename) {
     GtkTextBuffer *text_buffer =
-        gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
+        gtk_text_view_get_buffer(GTK_TEXT_VIEW(app_data->text_view));
     GtkTextIter start, end;
     gtk_text_buffer_get_start_iter(text_buffer, &start);
     gtk_text_buffer_get_end_iter(text_buffer, &end);
     char *text = gtk_text_buffer_get_text(text_buffer, &start, &end, TRUE);
 
-    FILE *file = fopen(data->filename, "w");
+    FILE *file = fopen(app_data->filename, "w");
     if (file) {
       fprintf(file, "%s", text);
       fclose(file);
       g_free(text);
-      data->modified = FALSE;
+      app_data->modified = FALSE;
     } else {
       g_print("Error saving file.\n");
       g_free(text);
     }
   } else {
-    save_as_file(data);
+    save_as_file(app_data);
   }
 }
 
-void new_file(AppData *data) {
+void new_file() {
   GtkTextBuffer *text_buffer =
-      gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
+      gtk_text_view_get_buffer(GTK_TEXT_VIEW(app_data->text_view));
   gtk_text_buffer_set_text(text_buffer, "", -1);
 
-  if (data->filename) {
-    g_free(data->filename);
-    data->filename = NULL;
+  if (app_data->filename) {
+    g_free(app_data->filename);
+    app_data->filename = NULL;
   }
-  data->modified = FALSE;
-  update_status_bar(data);
+  app_data->modified = FALSE;
+  update_status_bar(text_buffer, app_data->status_bar);
 }
 
-gboolean confirm_quit(AppData *data) {
-  if (!data->modified) {
+gboolean confirm_quit() {
+  if (!app_data->modified) {
     return TRUE; // Allow window to be closed
   } else {
     GtkWidget *dialog = gtk_message_dialog_new(
-        GTK_WINDOW(data->window),
+        GTK_WINDOW(app_data->window),
         GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT, GTK_MESSAGE_WARNING,
         GTK_BUTTONS_YES_NO,
         "You have unsaved changes. "
@@ -204,8 +204,8 @@ gboolean confirm_quit(AppData *data) {
   }
 }
 
-gboolean on_delete_event(GtkWidget *widget, GdkEvent *event, AppData *data) {
-  if (confirm_quit(data)) {
+gboolean on_delete_event(GtkWidget *widget, GdkEvent *event) {
+  if (confirm_quit(app_data)) {
     gtk_main_quit();
     return FALSE; // Let the window close
   } else {
@@ -213,35 +213,37 @@ gboolean on_delete_event(GtkWidget *widget, GdkEvent *event, AppData *data) {
   }
 }
 
-void on_text_changed(GtkWidget *widget, AppData *data) {
-  data->modified = TRUE;
-  update_status_bar(data);
+void on_text_changed(GtkWidget *widget) {
+  app_data->modified = TRUE;
+  update_status_bar(
+      gtk_text_view_get_buffer(GTK_TEXT_VIEW(app_data->text_view)),
+      app_data->status_bar);
 }
 
-gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, AppData *data) {
+gboolean on_key_press(GtkWidget *widget, GdkEventKey *event) {
   if ((event->state & GDK_CONTROL_MASK) && (event->keyval == GDK_KEY_s)) {
-    save_file(data);
+    save_file();
     return TRUE;
   }
   if ((event->state & GDK_CONTROL_MASK) && (event->keyval == GDK_KEY_n)) {
-    new_file(data);
+    new_file();
     return TRUE;
   }
   if ((event->state & GDK_CONTROL_MASK) && (event->keyval == GDK_KEY_o)) {
-    open_file(data);
+    open_file();
     return TRUE;
   }
   return FALSE;
 }
 
-void on_font_selected(GtkWidget *dialog, gint response_id, AppData *data) {
+void on_font_selected(GtkWidget *dialog, gint response_id) {
   if (response_id == GTK_RESPONSE_OK) {
     char *font_name = gtk_font_chooser_get_font(GTK_FONT_CHOOSER(dialog));
     if (font_name) {
       PangoFontDescription *font_desc =
           pango_font_description_from_string(font_name);
       if (font_desc) {
-        gtk_widget_override_font(data->text_view, font_desc);
+        gtk_widget_override_font(app_data->text_view, font_desc);
         pango_font_description_free(font_desc);
       } else {
         g_print("Error parsing font description.\n");
@@ -250,44 +252,46 @@ void on_font_selected(GtkWidget *dialog, gint response_id, AppData *data) {
     }
   }
   gtk_widget_destroy(dialog);
-  data->font_selection_dialog = NULL;
+  app_data->font_selection_dialog = NULL;
 }
 
-void open_font_dialog(AppData *data) {
-  if (data->font_selection_dialog) {
-    gtk_widget_destroy(data->font_selection_dialog);
+void open_font_dialog() {
+  if (app_data->font_selection_dialog) {
+    gtk_widget_destroy(app_data->font_selection_dialog);
   }
 
-  data->font_selection_dialog =
-      gtk_font_chooser_dialog_new("Select Font", GTK_WINDOW(data->window));
+  app_data->font_selection_dialog =
+      gtk_font_chooser_dialog_new("Select Font", GTK_WINDOW(app_data->window));
 
-  g_signal_connect(data->font_selection_dialog, "response",
-                   G_CALLBACK(on_font_selected), data);
-  gtk_widget_show_all(data->font_selection_dialog);
+  g_signal_connect(app_data->font_selection_dialog, "response",
+                   G_CALLBACK(on_font_selected), NULL);
+  gtk_widget_show_all(app_data->font_selection_dialog);
 }
 
-void on_cursor_position_changed(GtkWidget *text_view, AppData *data) {
-  update_status_bar(data);
+void on_cursor_position_changed(GtkWidget *text_view) {
+  GtkTextBuffer *text_buffer =
+      gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+  update_status_bar(text_buffer, app_data->status_bar);
 }
 
 int main(int argc, char *argv[]) {
   gtk_init(&argc, &argv);
 
-  AppData *data = g_new0(AppData, 1);
-  data->filename = NULL;
-  data->modified = FALSE;
-  data->font_selection_dialog = NULL;
+  app_data = g_new0(AppData, 1);
+  app_data->filename = NULL;
+  app_data->modified = FALSE;
+  app_data->font_selection_dialog = NULL;
 
-  data->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title(GTK_WINDOW(data->window), "GTK Notepad");
-  gtk_window_set_default_size(GTK_WINDOW(data->window), 600, 400);
-  g_signal_connect(data->window, "delete-event", G_CALLBACK(on_delete_event),
-                   data);
+  app_data->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(app_data->window), "GTK Notepad");
+  gtk_window_set_default_size(GTK_WINDOW(app_data->window), 600, 400);
+  g_signal_connect(app_data->window, "delete-event",
+                   G_CALLBACK(on_delete_event), app_data);
 
-  gtk_window_set_resizable(GTK_WINDOW(data->window), FALSE);
+  gtk_window_set_resizable(GTK_WINDOW(app_data->window), FALSE);
 
   GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
-  gtk_container_add(GTK_CONTAINER(data->window), vbox);
+  gtk_container_add(GTK_CONTAINER(app_data->window), vbox);
 
   GtkWidget *menubar = gtk_menu_bar_new();
   gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, FALSE, 0);
@@ -298,20 +302,20 @@ int main(int argc, char *argv[]) {
   gtk_menu_shell_append(GTK_MENU_SHELL(menubar), fileitem);
 
   GtkWidget *newitem = gtk_menu_item_new_with_label("New");
-  g_signal_connect_swapped(newitem, "activate", G_CALLBACK(new_file), data);
+  g_signal_connect_swapped(newitem, "activate", G_CALLBACK(new_file), NULL);
   gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), newitem);
 
   GtkWidget *openitem = gtk_menu_item_new_with_label("Open");
-  g_signal_connect_swapped(openitem, "activate", G_CALLBACK(open_file), data);
+  g_signal_connect_swapped(openitem, "activate", G_CALLBACK(open_file), NULL);
   gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), openitem);
 
   GtkWidget *saveitem = gtk_menu_item_new_with_label("Save");
-  g_signal_connect_swapped(saveitem, "activate", G_CALLBACK(save_file), data);
+  g_signal_connect_swapped(saveitem, "activate", G_CALLBACK(save_file), NULL);
   gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), saveitem);
 
   GtkWidget *saveasitem = gtk_menu_item_new_with_label("Save As...");
   g_signal_connect_swapped(saveasitem, "activate", G_CALLBACK(save_as_file),
-                           data);
+                           NULL);
   gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), saveasitem);
 
   GtkWidget *separator = gtk_separator_menu_item_new();
@@ -319,7 +323,7 @@ int main(int argc, char *argv[]) {
 
   GtkWidget *quititem = gtk_menu_item_new_with_label("Quit");
   g_signal_connect_swapped(quititem, "activate", G_CALLBACK(on_delete_event),
-                           data);
+                           NULL);
   gtk_menu_shell_append(GTK_MENU_SHELL(filemenu), quititem);
 
   GtkWidget *editmenu = gtk_menu_new();
@@ -329,40 +333,41 @@ int main(int argc, char *argv[]) {
 
   GtkWidget *fontitem = gtk_menu_item_new_with_label("Font...");
   g_signal_connect_swapped(fontitem, "activate", G_CALLBACK(open_font_dialog),
-                           data);
+                           NULL);
   gtk_menu_shell_append(GTK_MENU_SHELL(editmenu), fontitem);
 
-  data->text_view = gtk_text_view_new();
-  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(data->text_view), GTK_WRAP_WORD);
+  app_data->text_view = gtk_text_view_new();
+  gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(app_data->text_view),
+                              GTK_WRAP_WORD);
   GtkTextBuffer *text_buffer =
-      gtk_text_view_get_buffer(GTK_TEXT_VIEW(data->text_view));
-  g_signal_connect(text_buffer, "changed", G_CALLBACK(on_text_changed), data);
-  g_signal_connect(data->text_view, "cursor-changed",
-                   G_CALLBACK(on_cursor_position_changed), data);
+      gtk_text_view_get_buffer(GTK_TEXT_VIEW(app_data->text_view));
+  g_signal_connect(text_buffer, "changed", G_CALLBACK(on_text_changed), NULL);
+  g_signal_connect(app_data->text_view, "move-cursor",
+                   G_CALLBACK(on_cursor_position_changed), NULL);
 
   PangoFontDescription *font_desc =
       pango_font_description_from_string("Monospace");
-  gtk_widget_override_font(data->text_view, font_desc);
+  gtk_widget_override_font(app_data->text_view, font_desc);
   pango_font_description_free(font_desc);
 
   GtkWidget *scroll_window = gtk_scrolled_window_new(NULL, NULL);
   gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll_window),
                                  GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-  gtk_container_add(GTK_CONTAINER(scroll_window), data->text_view);
+  gtk_container_add(GTK_CONTAINER(scroll_window), app_data->text_view);
 
   gtk_box_pack_start(GTK_BOX(vbox), scroll_window, TRUE, TRUE, 0);
 
-  data->status_bar = gtk_label_new("Line: 1, Column: 1");
-  gtk_label_set_xalign(GTK_LABEL(data->status_bar), 0.0);
-  gtk_box_pack_start(GTK_BOX(vbox), data->status_bar, FALSE, FALSE, 0);
+  app_data->status_bar = gtk_label_new("Line: 1, Column: 1");
+  gtk_label_set_xalign(GTK_LABEL(app_data->status_bar), 0.0);
+  gtk_box_pack_start(GTK_BOX(vbox), app_data->status_bar, FALSE, FALSE, 0);
 
-  g_signal_connect(data->window, "key-press-event", G_CALLBACK(on_key_press),
-                   data);
+  g_signal_connect(app_data->window, "key-press-event",
+                   G_CALLBACK(on_key_press), NULL);
 
-  gtk_widget_show_all(data->window);
+  gtk_widget_show_all(app_data->window);
 
   if (argc > 1) {
-    load_file(data, argv[1]);
+    load_file(argv[1]);
   }
 
   gtk_main();
